7a9e1070cb704907b17bd5f770af428d2119b5f9
Adrian Szczechura <Adrian.Szczechura@stepstone.com>
2022-12-01 18:02:02 +0100

    chore: add extra test case to args_count

--- python/args-count/test_args_count.py
+++ python/args-count/test_args_count.py
@@ -3,3 +3,7 @@ import args_count
 
 def test_args_count():
     assert args_count.args_count(1, "ew", 3213, "312", 49) == 5
+
+
+def test_args_count():
+    assert args_count.args_count(32, a1=12) == 2
