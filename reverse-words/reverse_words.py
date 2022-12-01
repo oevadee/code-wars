7fb65167875821bbdd4788ba4ade9fce2a5462ad
Adrian Szczechura <Adrian.Szczechura@stepstone.com>
2022-12-01 11:27:49 +0100

    feat: resolve reverse_words python kata

---
+++ reverse-words/reverse_words.py
@@ -0,0 +1,12 @@
+def reverse_words(text: str) -> str:
+    newArr = text.split(' ')
+    reversed_word = ''
+    index = -1
+    for word in newArr:
+        index += 1
+        if (index == len(newArr) - 1):
+            reversed_word += word[::-1]
+        else: reversed_word += word[::-1] + ' '
+    return reversed_word
+
+print(reverse_words('The quick brown fox jumps over the lazy dog.'))
